6fc1ee1845c86936409c3b6ac7f377acaf392dbd
Hugo Bossard <hbossard01@gmail.com>
2024-09-22 14:18:09 +0200

    Ajout : debut du routing et création du premier fichier de service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then((m) => m.PageNotFoundModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageInfoRobotsComponent } from './pages/page-info-robots/page-info-robots.component';
+import { PageListRobotsComponent } from './pages/page-list-robots/page-list-robots.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageListRobotsComponent },
+  { path: '/robots/:id', component: PageInfoRobotsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
